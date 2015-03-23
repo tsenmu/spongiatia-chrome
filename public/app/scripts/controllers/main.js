f3d0cdab11a07ad3a7deffd56fedea097751a33b
Jingjie Zheng <i@tsenmu.com>
2015-03-22 20:56:49 -0400

    finished formatting

--- public/app/scripts/controllers/main.js
+++ public/app/scripts/controllers/main.js
@@ -2,17 +2,18 @@
 
 
 angular.module('spongiatiaApp')
-  .controller('MainCtrl', function ($scope, $sanitize, SpongiatiaService, marked) {
-    $scope.suggestions = [];
-    SpongiatiaService.onMessage.addListener(function(msg) {
-      $scope.$apply(function() {
-          $scope.suggestions = msg.suggestions;
-          alert($scope.suggestions[0].question.body_markdown);
-           $scope.suggestions = _.map($scope.suggestions, function(suggestion) {
-             suggestion.question.body = marked($sanitize(suggestion.question.body_markdown));
-             suggestion.answer.body = marked($sanitize(suggestion.accepted_answer.body_markdown));
-             return suggestion;
-           });
+.controller('MainCtrl', function ($scope, $sanitize, SpongiatiaService, marked) {
+  $scope.suggestions = [];
+  SpongiatiaService.onMessage.addListener(function(msg) {
+    $scope.$apply(function() {
+      $scope.suggestions = msg.suggestions;
+      $scope.suggestions = _.map($scope.suggestions, function(suggestion) {
+        suggestion.question.title = suggestion.question.title;
+        suggestion.question.body = _.unescape(marked(suggestion.question.body_markdown));
+        suggestion.answer = {};
+        suggestion.answer.body = _.unescape(marked(suggestion.accepted_answer.body_markdown));
+        return suggestion;
       });
     });
   });
+});
